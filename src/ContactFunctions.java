ab506b9e67b9d4c0104190143b7b43bc3f30e8d3
Anthony Stevens <aastevens1126@gmail.com>
2022-10-14 12:48:51 -0500

    Contact class complete

--- src/ContactFunctions.java
+++ src/ContactFunctions.java
@@ -1,5 +1,20 @@
-
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class ContactFunctions{
 
+//    public static void addContact(Path p, Contact contact) {
+//        String contactInfo = String.format("\n%s | %s", contact.getName(), contact.getPhoneNumber());
+//
+//        try {
+//            Files.write(p, contactInfo.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+//            System.out.println("Contact added successfully!");
+//
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
 }
