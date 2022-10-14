998ec86f1c3489019fc7ee4992dbbd3b5ab4a1f1
Anthony Stevens <aastevens1126@gmail.com>
2022-10-14 14:40:17 -0500

    Add Contact functionality

--- src/ContactFunctions.java
+++ src/ContactFunctions.java
@@ -2,19 +2,26 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactFunctions{
 
-//    public static void addContact(Path p, Contact contact) {
-//        String contactInfo = String.format("\n%s | %s", contact.getName(), contact.getPhoneNumber());
-//
-//        try {
-//            Files.write(p, contactInfo.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
-//            System.out.println("Contact added successfully!");
-//
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            e.printStackTrace();
-//        }
-//    }
+
+    public static List<String> getData(Path p){
+        List<String> contacts = null;
+        try{
+            contacts = Files.readAllLines(p);
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return contacts;
+    }
+    public static void addContact(List<String> contacts, Contact contact) {
+        String contactInfo = String.format("\n%s %s", contact.getName(), contact.getPhoneNumber());
+        contacts.add(contactInfo);
+    }
 }
